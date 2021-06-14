bcc82cc4c738ca1ee99dcba215f4cd1f7d667d08
Paul Enascut <enstpaul@gmail.com>
2021-06-14 19:46:49 +0300

    Fill build branch

--- README.md
+++
@@ -1,8 +0,0 @@
-# Star Match Game
-Custom-tailored implementation of the well-known "Star Match" game, made with ReactJS
-
-https://paul-enascut.github.io/Star-Match-Game/
-
-![Screenshot_3](https://user-images.githubusercontent.com/18511720/121924807-eba71d00-cd44-11eb-8676-fbe43d8e8b64.png)
-
-![Screenshot_2](https://user-images.githubusercontent.com/18511720/121924817-ecd84a00-cd44-11eb-9b26-3cb166388176.png)
---
+++ precache-manifest.3372ec5d9db3157d42de95507aa0afa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dedc06571b4976d4f2d4b76ecc18d0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b3d6df01a630e427e55",
+    "url": "/static/css/main.d750ec83.chunk.css"
+  },
+  {
+    "revision": "c9adc034e7d00ac3b148",
+    "url": "/static/js/2.09e0cfb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09e0cfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3d6df01a630e427e55",
+    "url": "/static/js/main.d4ed32bd.chunk.js"
+  },
+  {
+    "revision": "16c0f175fe3359e06bfe",
+    "url": "/static/js/runtime-main.7d4ba6f4.js"
+  }
+]);
